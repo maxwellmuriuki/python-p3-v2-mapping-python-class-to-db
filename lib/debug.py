084166e14451969f2e9050e95ae52912829885cf
Your Name <usamax089@gmail.com>
2024-12-19 22:21:20 +0300

    first   commit

--- lib/debug.py
+++ lib/debug.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python3
 #lib/testing/debug.py
 
+
 from __init__ import CONN, CURSOR
 from department import Department
 
 import ipdb
+
+Department.drop_table()
+Department.create_table()
+
+payroll = Department.create("Payroll", "Building A, 5th Floor")
+print(payroll)
+
+hr = Department.create("Human Resources", "Building C, East Wing")
+print(hr)
 
 ipdb.set_trace()
